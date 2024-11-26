0fd92fe21e8fb4b288363cc9cb55d513e4aa41cf
Anfang Liu <lafhhh@outlook.com>
2024-11-26 17:09:12 +0200

    courseinfo step 5

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -34,33 +34,36 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {props.total[0].exercises+props.total[1].exercises+props.total[2].exercises}</p>
+      <p>Number of exercises {props.total[0].exercises + props.total[1].exercises + props.total[2].exercises}</p>
     </div>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    { 
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    { 
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    { 
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   return (
     <div>
-      <Header course={course}> </Header>
-      <Content part={parts}></Content>
-      <Total total={parts}></Total>
+      <Header course={course.name}> </Header>
+      <Content part={course.parts}></Content>
+      <Total total={course.parts}></Total>
     </div>
   )
 }
