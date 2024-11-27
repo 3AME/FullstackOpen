afd5b20a6ec2681899eb230c785c2b3967e97918
Anfang Liu <lafhhh@outlook.com>
2024-11-27 22:05:05 +0200

    unicafe step3

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -7,27 +7,38 @@ const Button = (props) => {
   )
 }
 
+const Statistics = (props) => {
+  const good = props.good
+  const neutral = props.neutral
+  const bad = props.bad
+
+  const all = good + neutral + bad
+  const average = (good * 1 + neutral * 0 + bad * -1) / all
+  const positive = good / all * 100
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  )
+}
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleGood = () => {
-    setGood(good + 1)
-  }
+  const handleGood = () => setGood(good + 1)
 
-  const handleNeutral = () => {
-    setNeutral(neutral + 1)
-  }
 
-  const handleBad = () => {
-    setBad(bad + 1)
-  }
+  const handleNeutral = () => setNeutral(neutral + 1)
 
-  const all = good + neutral + bad
-  const average = (good * 1 + neutral * 0 + bad * -1) / all
-  const positive = good / all * 100
-  // console.log(positive.toFixed(14))
+
+  const handleBad = () => setBad(bad + 1)
 
   return (
     <div>
@@ -36,13 +47,7 @@ const App = () => {
       <Button onClick={handleNeutral} text='neutral' />
       <Button onClick={handleBad} text='bad' />
 
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
