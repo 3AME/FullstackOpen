a54a937ebda595f341ad7f198240741624ee88c9
Anfang Liu <lafhhh@outlook.com>
2024-11-27 22:26:09 +0200

    unicafe step 6

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -7,9 +7,12 @@ const Button = (props) => {
   )
 }
 
-const StatisticLine = (props) =>{
-  return(
-    <p>{props.text} {props.value}</p>
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   )
 }
 const Statistics = (props) => {
@@ -21,8 +24,8 @@ const Statistics = (props) => {
   const average = (good * 1 + neutral * 0 + bad * -1) / all
   const positive = good / all * 100 + " %"
 
-  if(good === 0 && neutral === 0 && bad === 0) {
-    return(
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
       <div>
         No feedback given
       </div>
@@ -30,14 +33,16 @@ const Statistics = (props) => {
   }
 
   return (
-    <div>
-      <StatisticLine text="good" value = {good}/>
-      <StatisticLine text="neutral" value = {neutral}/>
-      <StatisticLine text="bad" value = {bad}/>
-      <StatisticLine text="all" value = {all}/>
-      <StatisticLine text="average" value = {average}/>
-      <StatisticLine text="positive" value = {positive}/>
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
+      </tbody>
+    </table>
   )
 }
 const App = () => {
